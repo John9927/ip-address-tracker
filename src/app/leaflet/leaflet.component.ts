048e4c282371e007edc482c563a96d45656a5627
Giovanni Izzi <giovanni9972@libero.it>
2021-06-04 09:29:40 +0200

    hosting firebase

--- src/app/leaflet/leaflet.component.ts
+++ src/app/leaflet/leaflet.component.ts
@@ -1,7 +1,6 @@
 import { GetIpService } from './../services/get-ip.service';
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer, icon, marker, polyline } from 'leaflet';
-import * as L from 'leaflet';
+
 @Component({
   selector: 'app-leaflet',
   templateUrl: './leaflet.component.html',
